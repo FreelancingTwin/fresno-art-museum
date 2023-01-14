503b6847b31811ff9040023888a222222d45be71
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-01-14 21:43:56 +0530

    some cleanup, and add max-width to timings

--- src/views/components/Navbar.tsx
+++ src/views/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Dropdown from './Dropdown'
 import { RxHamburgerMenu, RxCross1 } from 'react-icons/rx'
 
 const Navbar = () => {
@@ -7,19 +6,28 @@ const Navbar = () => {
     
     const hamburgerToggle = () => {
         toggleDropdown()
-      if( hamburger.type.name === 'RxHamburgerMenu'){
+      if(hamburger.type.name === 'RxHamburgerMenu'){
         setHamburger(<RxCross1/>)
         } else {
         setHamburger(<RxHamburgerMenu/>)
         
       }
     }
-
+    let hiddenDropdown = document.getElementsByClassName('dropdown')
+    console.log(hiddenDropdown[0])
     function toggleDropdown(){
-      document.getElementsByClassName('dropdown')[0]
+              hiddenDropdown[0]
               .classList
               .toggle('dropdown-hidden')
     }
+    // hiddenDropdown[0].addEventListener('blur', ()=>{
+    //   console.log('araaraara')
+    //   hiddenDropdown[0].classList.add('dropdown-hidden')
+    // })
+    // document.body.addEventListener('onfocus', ()=>{
+    //   console.log('araaraara')
+    //   // hiddenDropdown[0].classList.add('dropdown-hidden')
+    // })
   return (
     <div className="parentNavbar">
     <div className='navbar'>
