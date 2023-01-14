ce418e8f09a3f2d7db74eb2425d7049258c17a04
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-01-15 00:02:36 +0530

    style the program components

--- src/views/components/Programs.tsx
+++ src/views/components/Programs.tsx
@@ -5,12 +5,12 @@ const programs = () => {
     <div className="programs">
       <section className="event">
           <img src="/assets/gallery/installation-view-from.jpg" alt="" className='programImage'/>
-          <h1 className='programHeading'>Fresno Art museum welcomes you to experience </h1>
+          <p className='programHeading'>Fresno Art museum welcomes you</p>
           <p className='programDate'>January 26 - 28, 2023</p>
       </section>
       <section className="event">
           <img src="/assets/gallery/fresno-art-museum.jpg" alt="" className='programImage'/>
-          <h1 className='programHeading'>Fresno Art museum welcomes you to experience </h1>
+          <p className='programHeading'>Fresno Art museum welcomes you to experience </p>
           <p className='programDate'>January 26 - 28, 2023</p>
       </section>
    
@@ -18,12 +18,12 @@ const programs = () => {
        
       <section className="event">
           <img src="/assets/gallery/p3.jpg" alt="" className='programImage'/>
-          <h1 className='programHeading'>Fresno Art museum welcomes you to experience </h1>
+          <p className='programHeading'>Fresno Art museum welcomes you to experience </p>
           <p className='programDate'>January 26 - 28, 2023</p>
       </section>
       <section className="event">
           <img src="/assets/gallery/photo1jpg.jpg" alt="" className='programImage'/>
-          <h1 className='programHeading'>Fresno Art museum welcomes you to experience </h1>
+          <p className='programHeading'>Fresno Art museum welcomes you to experience </p>
           <p className='programDate'>January 26 - 28, 2023</p>
       </section>
       <div className="calendar"><h1>See program Calendar</h1></div>
