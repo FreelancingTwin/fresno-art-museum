47acbfef4317c1b155964ceacaec6a6e7aae2236
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-01-16 00:18:33 +0530

    fix navbar state bug by assigning keys to icon components

--- src/views/components/Navbar.tsx
+++ src/views/components/Navbar.tsx
@@ -2,37 +2,31 @@ import React, { useState } from 'react'
 import { RxHamburgerMenu, RxCross1 } from 'react-icons/rx'
 
 const Navbar = () => {
-    const [hamburger, setHamburger] = useState(<RxHamburgerMenu/>)
-    
-    const hamburgerToggle = () => {
-        toggleDropdown()
-      if(hamburger.type.name === 'RxHamburgerMenu'){
-        setHamburger(<RxCross1/>)
-        } else {
-        setHamburger(<RxHamburgerMenu/>)
-        
-      }
-    }
+    const [hamburger, setHamburger] = useState(()=><RxHamburgerMenu key='1'/>)
+   
     let hiddenDropdown = document.getElementsByClassName('dropdown')
-    console.log(hiddenDropdown[0])
+    // console.log(hiddenDropdown)
     function toggleDropdown(){
               hiddenDropdown[0]
               .classList
               .toggle('dropdown-hidden')
+              // console.log('Clicked hamburger')
+
+      if(hamburger.key === '1'){
+      setHamburger(<RxCross1 key='2'/>)
+        } else if(hamburger.key === '2'){
+        setHamburger(<RxHamburgerMenu key='1' />)
+      }
+
+      console.log(hamburger.key)
     }
-    // hiddenDropdown[0].addEventListener('blur', ()=>{
-    //   console.log('araaraara')
-    //   hiddenDropdown[0].classList.add('dropdown-hidden')
-    // })
-    // document.body.addEventListener('onfocus', ()=>{
-    //   console.log('araaraara')
-    //   // hiddenDropdown[0].classList.add('dropdown-hidden')
-    // })
+
+     
   return (
     <div className="parentNavbar">
     <div className='navbar'>
       <img className="logo" src="../assets/logo.png" alt="logo"/>
-      <h1 className='hamburger' onClick={hamburgerToggle}>{hamburger}</h1>
+      <h1 className='hamburger' onClick={toggleDropdown}>{hamburger}</h1>
     </div>
     {/* <Dropdown/> */}
     </div>
